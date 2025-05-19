eb7c966c4f568cce391bdd3ebb3a9ea3142bb6ac
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-19 02:09:29 -0500

    product contorler refactor

--- server/src/controller/productConrtoller.ts
+++ server/src/controller/productConrtoller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import RequestSchema from "../schema/request";
+import ResponseSchema from "../schema/request";
 import { product } from "../data";
 import {ORM, searchItem} from "../utils/orm";
 import { transformQuery } from "../utils/transformQuery";
@@ -7,11 +7,11 @@ import { transformQuery } from "../utils/transformQuery";
 export function getProducts(req: Request, res: Response) {
     let searchQ : searchItem[] = transformQuery(req.query, {})
     const result = ORM.get({model: product, search: searchQ, sortBy: [{key: "dateAdded", order: "desc"}]}) 
-    res.json(new RequestSchema(result))
+    res.json(new ResponseSchema(result))
 }
 
 export function getProductsByName(req: Request, res: Response) {
     let searchQ : searchItem[] = transformQuery(req.query, {q:"name"})
     const result = ORM.get({model: product, search: searchQ, sortBy: [{key: "dateAdded", order: "desc"}]})
-    res.json(new RequestSchema(result))
+    res.json(new ResponseSchema(result))
 }
