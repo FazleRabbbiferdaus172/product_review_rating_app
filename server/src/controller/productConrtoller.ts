ebaff1e70d7b0091b5d796c4400a9635090a555a
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-18 19:26:41 -0500

    initial orm implementation

--- server/src/controller/productConrtoller.ts
+++ server/src/controller/productConrtoller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import RequestSchema from "../schema/request";
 import { data } from "../data";
+import ORM from "../utils/orm";
 
 export function getProducts(req: Request, res: Response) {
-    res.json(new RequestSchema(data))
+    const result = ORM.get({model: data}) 
+    res.json(new RequestSchema(result))
 }
---
+++ server/src/utils/orm.ts
@@ -0,0 +1,98 @@
+type searchItem = {key: string, query: string}
+type sortByItem = {key: string, order: "asce"| "desc" |false}
+
+type GetParams = {
+    model: object[],
+    offset?: number,
+    limit?: number,
+    search?: searchItem[] | false,
+    sortBy?: sortByItem[] | false
+}
+
+class ORM {
+
+    static sortAsce(records: object[], key: string) {
+        records.sort((r1: Record<string, any>, r2: Record<string, any>) => {
+            if (r1[key] < r2[key]) return -1
+            if (r1[key] > r2[key]) return 1
+            else return 0
+        })
+    }
+
+    static sortDesc(records: object[], key: string) {
+        records.sort((r1: Record<string, any>, r2: Record<string, any>) => {
+            if (r1[key] < r2[key]) return 1
+            if (r1[key] > r2[key]) return -1
+            else return 0
+        })
+    }
+
+    static orderBy(records: object[], key: string, order: string| false) {
+        if (order === 'desc') ORM.sortDesc(records, key)
+        else ORM.sortAsce(records, key)  
+    }
+
+    static searchRecord(records: object[], search:searchItem[]): object[] {
+        let result = records.filter((record: Record<string, any>) =>
+            search
+                .map((si) => record[si.key] === si.query)
+                .every(Boolean)
+        );
+        return result
+    }
+
+    static get({ model, offset = 0, limit = 10, search = false, sortBy = false }: GetParams): object[] {
+            let result: object[] = model
+
+            if (search !== false) {
+                result = ORM.searchRecord(result, search)
+            }
+
+            if (sortBy) {
+                sortBy.forEach(
+                    (so:Record<string, any>) => {
+                        ORM.orderBy(result, so.key, so.order)
+                    }
+                )
+            }
+
+            if(offset) {
+                offset = Math.min(result.length, offset)
+            }
+
+            if (limit) {
+                limit = Math.min(limit, result.length - offset)
+            }
+            return result.splice(offset, limit)
+    }
+
+    static create(record: Record<string, any>, model:object[]): boolean {
+        model.push(record)
+        return true
+    }
+
+    static delete(record: Record<string, any>, model: object[]): boolean {
+        const index = model.indexOf(record)
+        if (index !== -1) {
+            model.slice(index, 1)
+            return true
+        }
+        return false
+    }
+
+    static update(record: Record<string, any>, newRecord: Record<string, any>, model: object[]): boolean {
+        ORM.delete(record, model)
+        ORM.create(newRecord, model)
+        return true
+    }
+}
+
+// Example of using ORM
+// console.log(ORM.get({
+//         model: [{a: "b"}, {a: "a"}, {a: "x"},{a: "a"}],
+//         search: [],
+//         sortBy: [{key: "a", order: "asce"}]
+//         }
+//     ))
+
+export default ORM
