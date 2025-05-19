14a4ca8e9fa6a2c02ae1dc418e8011e66e0556ec
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-18 23:28:04 -0500

    utils to transform query to search items

---
+++ server/src/utils/transformQuery.ts
@@ -0,0 +1,14 @@
+import { searchItem } from "./orm";
+
+type KeyUpdate = {key: string, newKey: string}
+
+export function transformQuery(query: {}, nameUpdate: Record<string, string>): searchItem[] {
+    let search : searchItem[] = [];
+    Object.entries(query).forEach(([k, v]) => 
+        {
+         let item: searchItem = k in nameUpdate ? {key: nameUpdate[k], query: v as string} : {key: k, query: v as string}
+         search.push(item)
+        }
+    );
+    return search
+}
