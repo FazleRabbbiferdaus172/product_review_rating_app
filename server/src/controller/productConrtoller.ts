5c4520825c568812633cc0c474634a2062e6c34a
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-18 23:28:46 -0500

    product controller with orms

--- server/src/controller/productConrtoller.ts
+++ server/src/controller/productConrtoller.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
 import RequestSchema from "../schema/request";
-import { data } from "../data";
-import ORM from "../utils/orm";
+import { product } from "../data";
+import {ORM, searchItem} from "../utils/orm";
+import { transformQuery } from "../utils/transformQuery";
 
 export function getProducts(req: Request, res: Response) {
-    const result = ORM.get({model: data}) 
+    let searchQ : searchItem[] = transformQuery(req.query, {})
+    const result = ORM.get({model: product, search: searchQ, sortBy: [{key: "dateAdded", order: "desc"}]}) 
+    res.json(new RequestSchema(result))
+}
+
+export function getProductsByName(req: Request, res: Response) {
+    let searchQ : searchItem[] = transformQuery(req.query, {q:"name"})
+    const result = ORM.get({model: product, search: searchQ, sortBy: [{key: "dateAdded", order: "desc"}]})
     res.json(new RequestSchema(result))
 }
