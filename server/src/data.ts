f8cd712a3637d36ceb90793453352f2e5e8217e5
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-18 23:27:36 -0500

    data updated

--- server/src/data.ts
+++ server/src/data.ts
@@ -1,8 +1,25 @@
-type Product = {id: number, name: string, category: string, dateAdded: string}
+type Product = {id: string, name: string, category: string, dateAdded: string}
+type Review = {id: string, author: string, comment: string, productId: string, rating: 0 | 1 | 2 | 3 | 4 | 5}
 
-export const data:Product[] = [
+export const product:Product[] = [
 ]
 
-for (let i=0; i < 100; i ++) {
-    data.push({id:1, name: "a", category: "a", dateAdded: "02/12/1999"})
+for (let i=1; i <= 100; i ++) {
+
+    product.push({id: `${i}`, name: "a", category: "a", dateAdded: `02/12/199${i}`})
+}
+
+
+export const review: Review[] = []
+
+for (let i=1; i <= 100; i++) {
+    review.push(
+        {
+            id: `${i}`,
+            author: "a",
+            comment: "a",
+            productId: `${i}`,
+            rating: 5
+        }
+    )
 }
