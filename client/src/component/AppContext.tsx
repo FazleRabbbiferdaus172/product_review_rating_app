572a2f6f70e654b0dccf7b17c2931ba432acc5ba
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-22 17:15:57 -0500

    refactored app provider that implements a way to handle relational data

--- client/src/component/AppContext.tsx
+++ client/src/component/AppContext.tsx
@@ -3,17 +3,46 @@ import { useEffect, useState } from "react";
 
 export const AppContext = createContext([])
 
+export const initialData = {count: 0, result: {data: []}}
+
 export const AppDataProvider = ({children}) => {
     let x = "hi"
-    let [items, setProducts] = useState({count: 0, result: {data: []}})
-    
-    async function loadProducts() {
-        let result = await fetch('http://localhost:3000/products')
-        let dataJson = await result.json()
-        setProducts(dataJson)
+    let [items, setProducts] = useState(initialData)
+
+
+    async function setData(setterFunc,data) {
+        setterFunc(data)
     }
 
-    useEffect(() => {loadProducts()}, [])
+    async function loadData(url, hasRelaionalData=false, relationalDataUrl, replaceWith, relationName) {
+        let data = await fetch(url)
+        let dataJson = await data.json()
+        if (hasRelaionalData) {
+            await loadNestedData(dataJson, relationalDataUrl, replaceWith, relationName)
+        }
+        debugger
+        return dataJson
+    }
+
+    async function setLoadData(url, setterFunc, hasRelaionalData=false, relationalDataUrl, replateWith, relationName ) {
+        const data = await loadData(url, hasRelaionalData, relationalDataUrl, replateWith, relationName)
+        setData(setterFunc, data)
+    }    
+
+    // async gatherRelationalData() {}
+
+    async function loadNestedData(data, relationalDataUrl, replaceWith, relationName) {
+        for (let d of data.result.data) {
+            let url = relationalDataUrl.replace('{}', d[replaceWith])
+            let relationalData = await loadData(url, false)
+            d[relationName] = relationalData
+        }
+    }
+
+    useEffect(() => {
+        setLoadData("http://localhost:3000/products", setProducts, true, "http://localhost:3000/products/{}/reviews", "id", "reviews")
+    }, [])
+
 
     return (
         <AppContext.Provider value={{x, items}}>
