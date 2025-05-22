4473e59cf372b4b1812e33628d378797d11cf5c1
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-22 00:40:31 -0500

    cors addition

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,15 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response,  } from 'express';
+import cors from 'cors'
 import productRouter from './productRoute';
 
 const app = express();
 const port: number = 3000;
 
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
+
+app.use(cors())
 app.use(express.json());
 app.use('/products', productRouter)
 
